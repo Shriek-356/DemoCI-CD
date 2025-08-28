b8b455cfd6f38c66efe6b89fe230d26b40b84274
Shriek-356 <khangvskiss@gmail.com>
2025-08-29 00:14:45 +0700

    a

---
+++ MuonTraSach/MuonTraSach/dao.py
@@ -0,0 +1,37 @@
+import json, os
+
+#
+# Không Phân quyền (Lấy file k có role)
+#
+
+# def auth_user(username, password):
+#     base_dir = os.path.dirname(__file__)
+#     file_path = os.path.join(base_dir, "data", "user.json")
+#     with open(file_path, encoding="utf8") as f:
+#         users = json.load(f)
+#         for u in users:
+#             if u["username"] == username and u["password"] == password:
+#                 return u
+#     return None
+
+
+
+
+#
+# Phân quyền (Lấy file có role)
+#
+
+def auth_user(username, password):
+    base_dir = os.path.dirname(__file__)
+    file_path = os.path.join(base_dir, "data", "user_role.json")
+    with open(file_path, encoding="utf8") as f:
+        users = json.load(f)
+        for u in users:
+            if u["username"] == username and u["password"] == password:
+                return u
+    return None
+
+
+
+if __name__ == '__main__':
+    print(auth_user("admin", "1"))
